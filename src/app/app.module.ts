626d533a65186a426ecac1ad4efb9f98f5810d3e
Anne Caroline <carolaragao94@gmail.com>
2022-07-13 10:25:24 -0300

    Criando cards para listagem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListApiComponent } from './components/list-api/list-api.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListApiComponent
   ],
   imports: [
     BrowserModule
